ae7d3f1656e2f39e8d167916f694463c69dc6f2c
Gabrielle <gabbybailu@gmail.com>
2022-12-08 09:47:28 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+//
+function linkedinLogin(){
+    console.log('linkedinLogin called');
+    var src="https://www.linkedin.com/in/gabrielle-duarte-a0aa37227/"
+    api_key: 'XXXXXXXXXXXXXXXX'
+    authorize: true
+    onLoad: OnLinkedInFrameworkLoad
+}
